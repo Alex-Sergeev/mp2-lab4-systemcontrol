5a2428c4dc718188e9447d959a506295b85afa71
Alex-Sergeev <serega-al_ss2@mail.ru>
2016-12-19 22:32:26 +0300

    Выволнен класс контроля системы

--- base/Labwork4_SystemControl/SystemControl.cpp
+++ base/Labwork4_SystemControl/SystemControl.cpp
@@ -1,2 +1,121 @@
 #include "TQueue.h"
+#include "SystemControl.h"
 #include "TNode.h"
+#include <iostream>
+
+using namespace std;
+
+void SystemControl::SystemAddObjective(int ObjTime, int ObjProc)
+{
+	try 
+	{
+		Queue.AddObjective(ObjProc, ObjTime);
+	}
+	catch (...)
+	{
+		ObjectiveSkipped++;
+	}
+}
+
+void SystemControl::ObjectiveToPerformance()
+{
+	if (FreeProcessors == 0)
+		return;
+	for (int i = 0; i < Queue.getCurrentSize(); i++)
+	{
+		if (Queue.GetObjective(i).Priority > 8)
+		{
+			if (Queue.GetObjective(i).ProcessorAmount <= FreeProcessors)
+			{
+				ProccessorAmount[AmountOfObjectives] = Queue.GetObjective(i).ProcessorAmount;
+				FreeProcessors = FreeProcessors - Queue.GetObjective(i).ProcessorAmount;
+				CurrentTimeOfObjectiveWork[AmountOfObjectives] = Queue.GetObjective(i).TactTime;
+				TimeOfObjectiveWork[AmountOfObjectives] = 0;
+				AmountOfObjectives++;
+				Queue.DeleteObjective(i);
+			}
+			else
+				return;
+		}
+		else
+			if (Queue.GetObjective(i).ProcessorAmount <= FreeProcessors)
+			{
+				ProccessorAmount[AmountOfObjectives] = Queue.GetObjective(i).ProcessorAmount;
+				FreeProcessors = FreeProcessors - Queue.GetObjective(i).ProcessorAmount;
+				TimeOfObjectiveWork[AmountOfObjectives] = Queue.GetObjective(i).TactTime;
+				CurrentTimeOfObjectiveWork[AmountOfObjectives] = 0;
+				AmountOfObjectives++;
+				Queue.DeleteObjective(i);
+			}
+	}
+}
+
+void SystemControl::FinishingObjective()
+{
+	for (int i = 0; i < AmountOfObjectives; i++)
+	{
+		if (CurrentTimeOfObjectiveWork[i] == TimeOfObjectiveWork[i])
+		{
+			for (int j = 0; j < AmountOfObjectives-1; j++)
+			{
+				FreeProcessors = FreeProcessors + ProccessorAmount[i];
+				ProccessorAmount[j] = ProccessorAmount[j + 1];
+				TimeOfObjectiveWork[j] = TimeOfObjectiveWork[j + 1];
+				CurrentTimeOfObjectiveWork[j] = CurrentTimeOfObjectiveWork[j + 1];
+			}
+			AmountOfObjectives--;
+			ObjectiveCompleted++;
+		}
+	}
+}
+
+void SystemControl::ShowResults()
+{
+	setlocale(LC_ALL, "Russian");
+	cout <<"Выполнено задач" << endl;
+	cout << ObjectiveCompleted << endl;
+	cout << "Число задач в очереди"<< endl;
+	cout <<Queue.getCurrentSize() << endl;
+	cout << "Число пропущенных задач"<< endl;
+	cout << ObjectiveSkipped << endl;
+	cout <<"Число тактов простоя" << endl;
+	cout << AmountOfIdleTacts << endl;
+}
+
+void SystemControl::ControlSystemSimulation()
+{
+	setlocale(LC_ALL, "Russian");
+	int  ObjProc;
+	int ObjTime;
+	for (int i = 0; i < WorkTime; i++)
+	{
+		FinishingObjective();
+		if ((1 / ((rand() % 5)+1)) > AddThreshold)
+		{
+			cout << "Введите зачаду:" << endl;
+			cout << "Кол-во процессоров для задачи" << endl;
+			do
+			{
+				cin >> ObjProc;
+				if ((ObjProc < 1) || (ObjProc > 64))
+					cout << "Кол-во пролцессоров должно быть больше 0 и меньше 64" << endl;
+				else
+					break;
+			} while (true);
+			cout << "Время выполнения(такты)" << endl;
+			do
+			{
+				cin >> ObjTime;
+				if ((ObjTime < 1) || (ObjTime > 11))
+					cout << "Кол-во тактов исполнения должно быть больше 0 и меньше 11" << endl;
+				else
+					break;
+			} while (true);
+			SystemAddObjective(ObjTime, ObjProc);
+		}
+		ObjectiveToPerformance();
+		AmountOfIdleTacts = AmountOfIdleTacts + FreeProcessors;
+	}
+	ObjectiveCompleted = ObjectiveCompleted + AmountOfObjectives;
+	ShowResults();
+}
--- base/Labwork4_SystemControl/SystemControl.h
+++ base/Labwork4_SystemControl/SystemControl.h
@@ -5,9 +5,36 @@
 
 class SystemControl
 {
-	int var;
+	int FreeProcessors;
+	int WorkTime;
+	int ObjectiveCompleted;
+	int ObjectiveSkipped;
+	int AmountOfIdleTacts;
+	double AddThreshold;
+	int AmountOfObjectives;
+	int TimeOfObjectiveWork[64];
+	int CurrentTimeOfObjectiveWork[64];
+	int ProccessorAmount[64];
+	TQueue Queue;
 
+public:
 
+	SystemControl(double addThreshold, int Time)
+	{
+		FreeProcessors = 64;
+		WorkTime = Time;
+		ObjectiveCompleted = 0;
+		ObjectiveSkipped = 0;
+		AmountOfIdleTacts = 0;
+		AmountOfObjectives = 0;
+		AddThreshold = addThreshold;
+	}
+
+	void SystemAddObjective(int ObjTime, int ObjProc);
+	void ObjectiveToPerformance();
+	void FinishingObjective();
+	void ShowResults();
+	void ControlSystemSimulation();
 };
 
 #endif
